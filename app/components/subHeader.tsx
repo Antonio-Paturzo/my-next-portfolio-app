bb3023c2fe8a59a52410807ba28071953bba7dbd
Antonio L Paturzo <antonio.paturzo@snhu.edu>
2023-05-18 16:41:25 -0700

    update

--- app/components/subHeader.tsx
+++ app/components/subHeader.tsx
@@ -4,8 +4,8 @@ export default async function subHeader() {
   const about = await getAboutMe();
   return (
     <div>
-      <div className="text-base font-medium leading-none text-white">{about.name}</div>
-      <div className="text-sm font-medium leading-none text-gray-400">{about.title}</div>
+      <div className="text-base font-medium leading-none text-white"></div>
+      <div className="text-sm font-medium leading-none text-gray-400"></div>
     </div>
   )
 }
