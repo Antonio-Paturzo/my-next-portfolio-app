706c1e20e668be4f63ff938d395b06b694f7e741
Antonio L Paturzo <antonio.paturzo@snhu.edu>
2023-05-18 21:56:47 -0700

    adding ts lint solution for async components

--- app/components/RenHeader.tsx
+++ app/components/RenHeader.tsx
@@ -3,6 +3,7 @@ import Header from './Header'
 export default function RenHeader() {
   return (
     <>
+    {/* @ts-expect-error Server Component */}
       <Header />
     </>
   )
--- app/components/RenProjects.tsx
+++ app/components/RenProjects.tsx
@@ -3,6 +3,7 @@ import Projects from "./Projects";
 export default function RenHeader() {
   return (
     <>
+    {/* @ts-expect-error Server Component */}
       <Projects />
     </>
   )
